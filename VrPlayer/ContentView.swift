ae43fb6c6f462cbce125f9eee160428048790135
ryuto-imai <ryuu.0806@gmail.com>
2021-03-05 03:04:38 +0900

    VR動画プレイヤーを作成

---
+++ VrPlayer/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  VrPlayer
+//
+//  Created by Ryuto Imai on 2021/02/28.
+//
+
+import SwiftUI
+import SceneKit
+
+struct ContentView: View {
+    let vrScene = VrScene()
+    
+    var body: some View {
+        SceneView(scene: vrScene)
+            .gesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged(vrScene.drag(value:))
+            )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
